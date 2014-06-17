69d8f509bc00d94dc31f6d0914f3776668ca3029
C. Moss <caly.moss@gmail.com>
2014-06-16 22:30:31 -0700

    added functions to render messages and create new chat rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,24 +5,43 @@ window.app = app;
 
 //in init, fumction on click, take message, room, and 
 //  username input and trigger app.send
-//function to create chat rooms
-//function to create messages
+//  function to create chat rooms
+//  function to create messages
 //addMessagesToDom function
 //  takes message.text and creates a div
 //  add username/friendname and chatRoom as span in message
 
 app.userName = prompt('Enter your username. Make it a good one!');
-app.chatRoom = {};
-app.friend = {};
+app.chatRooms = {};
+app.friends = {};
 
+app.renderMessage = function(message) {
 
+	var messageText = $("<div class='chatContent'>" + message.text+ "</div>");
 
-app.init = function() {
-	//event listeners
+	var messageUser = $("<span class='user'>" + message.username+ "</span>");
+	messageText.prepend(messageUser);
 
+	var messageRoom = $("<br/><br/><span class='room'>" + message.roomname + "</span>");
+	messageText.prepend(messageRoom);
+
+	var messageContainer = $("<div class='container'></div>");
+	messageContainer.append(messageText);
+
+  $(."chatMessagesDiv").append(messageContainer);
+
+}
+
+//create new chat room
+app.createNewRoom = function(name) {
+	if (!app.chatRooms.message.roomname) {
+		app.chatRooms.message.roomname = true;
+	}
 };
 
+//send messages to server
 app.send = function(message) {
+
 	$.ajax({
 		url: 'https://api.parse.com/1/classes/chatterbox',
 		type: 'POST',
@@ -35,20 +54,40 @@ app.send = function(message) {
 			console.error('chatbox: Failed to send message');
 		}
 	});
+
 };
+
+//get messages from server
 app.fetch = function() {
+
 	$.ajax({
 		url: 'https://api.parse.com/1/classes/chatterbox',
 		type:'GET',
-		success: function(data) {
-			//addMessagesToDom
+		success: function(chats) {
+			_.each(chats, function(chat) {
+				renderMessages(chat);
+			});
+		}
 	});
+
 };
 
-// message: {
-// 'username': 'shawndrost',
-// 'text': 'trololo',
-// 'roomname': '4chan'
-// };
+//click listeners for interaction with the html
+app.init = function() {
 
+	$('.sendMessages').on('click', function() {
+			var userInput = $('.messageInput').val();
+			var message = {
+			'username':app.userName,
+			'text': userInput,
+			'roomname': chatRoom
+			}; 
+	  app.send(message);
+	});
 
+	$(".createRoom").on("click", function() {
+			var newRoom = prompt("What do you want to call this room?");
+			createNewRoom(newRoom);
+	}); 
+
+};
