d5cea90601dbc8a15811c2b508c23297d5a8d801
C. Moss <caly.moss@gmail.com>
2014-06-17 20:07:13 -0700

    implemented adding rooms and friends and bolding friend messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,92 +2,139 @@ var app = {};
 //why do I need this below?? 
 window.app = app;
 
-
-//in init, fumction on click, take message, room, and 
-//  username input and trigger app.send
-//  function to create chat rooms
-//  function to create messages
-//addMessagesToDom function
-//  takes message.text and creates a div
-//  add username/friendname and chatRoom as span in message
-
 app.userName = prompt('Enter your username. Make it a good one!');
-app.chatRooms = {};
+app.server = 'https://api.parse.com/1/classes/chatterbox';
+app.chatRooms = {main: true};
 app.friends = {};
 
-app.renderMessage = function(message) {
+//click listeners for interaction with the html
+app.init = function() {
+	console.log("initted");
+	app.username = window.location.search.substr(10);
+	app.loadAll();
+	app.text = $('#messageInput');
+	app.roomMenu = $('#roomSelect');
 
-	var messageText = $("<div class='chatContent'>" + message.text+ "</div>");
+	$('.submit').on('click', app.handleSubmit);
 
-	var messageUser = $("<span class='user'>" + message.username+ "</span>");
-	messageText.prepend(messageUser);
+	$("#createRoom").on("click", app.addRoom);
 
-	var messageRoom = $("<br/><br/><span class='room'>" + message.roomname + "</span>");
-	messageText.prepend(messageRoom);
+	$('.chatUser').on('click', app.addFriend); 
 
-	var messageContainer = $("<div class='container'></div>");
-	messageContainer.append(messageText);
+	$('#refresh').on("click", function() {
+		app.clearMessages();
+		app.fetch();
+	});
+};
 
-  $(."chatMessagesDiv").append(messageContainer);
-
-}
-
-//create new chat room
-app.createNewRoom = function(name) {
-	if (!app.chatRooms.message.roomname) {
-		app.chatRooms.message.roomname = true;
-	}
+//compiles message content for sending
+app.handleSubmit = function(e) {
+	//this is for preventing default form behavior (refresh after submission)
+	e.preventDefault();
+	var message = {
+		'username': app.escaper(app.userName),
+		'text': app.escaper(app.text.val()),
+		'roomname': app.roomMenu.val()
+	}; 
+	app.text.val('');
+	app.send(message);
 };
 
 //send messages to server
 app.send = function(message) {
-
 	$.ajax({
-		url: 'https://api.parse.com/1/classes/chatterbox',
+		url: app.server,
 		type: 'POST',
 		data: JSON.stringify(message),
 		contentType: 'application/json',
 		success: function() {
 			console.log('chatterbox: Message sent');
 		},
-		error: function(data) {
+		error: function() {
 			console.error('chatbox: Failed to send message');
 		}
 	});
-
 };
 
-//get messages from server
+//get new messages from server
 app.fetch = function() {
-
 	$.ajax({
-		url: 'https://api.parse.com/1/classes/chatterbox',
+		url: app.server,
 		type:'GET',
-		success: function(chats) {
-			_.each(chats, function(chat) {
-				renderMessages(chat);
+		data: {order: '-createdAt'},
+		contentType: 'application/json',
+		success: function(json) {
+			_.each(json.results, function(message) {
+				app.addMessage(message);
 			});
+		},
+		complete: function (json) {
 		}
 	});
-
 };
 
-//click listeners for interaction with the html
-app.init = function() {
-
-	$('.sendMessages').on('click', function() {
-			var userInput = $('.messageInput').val();
-			var message = {
-			'username':app.userName,
-			'text': userInput,
-			'roomname': chatRoom
-			}; 
-	  app.send(message);
-	});
-
-	$(".createRoom").on("click", function() {
-			var newRoom = prompt("What do you want to call this room?");
-			createNewRoom(newRoom);
-	}); 
-
+//escape user input for incoming and outoging messages and usernames
+app.escaper = function(input) {
+	if (input !== undefined && input !== null) {
+	return input
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/\"/g, '&quot;')
+		.replace(/\'/g, '&#39;');
+	}
 };
+
+
+//add message to DOM once fetched
+app.addMessage = function(message) {
+
+		var cleanText = app.escaper(message.text);
+		var cleanUserName = app.escaper(message.username);
+	  var cleanRoomName = app.escaper(message.roomname) || undefined;
+
+		var messageText = $("<div class='chatContent'>" + cleanText + "</div>");
+		var messageUser = $("<span class='chatUser'>" + cleanUserName + ": " + "</span>");
+		var messageRoom = $("<br/><br/><span class='whichRoom'>" + "[" + cleanRoomName + "] " + "</span>");
+		var messageContainer = $("<div class='container'></div>");
+
+		messageText.prepend(messageUser);
+		messageText.prepend(messageRoom);
+		messageContainer.append(messageText);
+
+
+		$('#chats').append(messageContainer);
+};
+
+//clear messages from DOM
+app.clearMessages = function() {
+	$('#chats').empty();
+};
+
+//create new chat room
+app.addRoom = function() {
+	var newRoomName = prompt("What do you want to call this room?");
+	if (!app.chatRooms[newRoomName]) {
+		app.chatRooms[newRoomName] = true;
+	}
+	$("#roomSelect").append('<option value=' + newRoomName + '>' + newRoomName + '</option>');
+};
+
+//add friends
+app.addFriend = function (newFriend) {
+	if (!friends[newFriend]) {
+			friends[newFriend] = true;
+	}
+	$('.chatUser:contains(' + newFriend + ')').closest('.container').toggleClass('friend');
+}; 
+
+
+app.loadAll = function () {
+	app.fetch();
+	setInterval(app.fetch, 10000);
+};
+
+//create a friend class
+//show anything with friend class in bold
+//click a username, add it to friend list if not already there
+//add friend class to everything in friend list
